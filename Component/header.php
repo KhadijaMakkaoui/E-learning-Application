c57c608de5809ad7ecac413daccf6a97da676fbe
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-01-20 19:47:20 +0100

    Créer un fichier header.php dans le dossier Component Contenant le code php de la partie header

---
+++ Component/header.php
@@ -0,0 +1,21 @@
+<?php 
+$header=<<<navbar
+<nav class="navbar row">
+                    <div class="col-2 d-flex flex-row w-100  justify-content-between ">
+                        <div>
+                            <a class="link-secondary">
+                                <i class="fs-5 bi-caret-left-square"></i>
+                            </a>
+                        </div>
+                        <div class="input-group" style="width:170px;">
+                            <input type="text" class="form-control" placeholder="Search ..">
+                            <button class="btn btn-outline-secondary rounded-end me-2" type="button" id="button-addon2">
+                                <i class="fs-6 bi-search"></i>
+                            </button>
+                            <i class="fs-5 bi-bell text-secondary mt-1"></i>
+                        </div>
+
+                    </div>
+                </nav>
+navbar;
+?>

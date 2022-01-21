471664a1b88711c076348102f107a77bad528a17
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-01-21 13:07:48 +0100

    Créer un fichier ListStudents.php dans le dossier Components Contentant le tableau multidimontionnel avec les tableaux associatives contenant les données des apprenants

---
+++ Component/ListStudents.php
@@ -0,0 +1,11 @@
+<?php
+$studentsList = array (
+    ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Khadija","Email"=> "makkaoui@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
+    ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Noura","Email"=> "Noura@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
+    ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Lamya","Email"=> "lamya@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
+    ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Ahmed","Email"=> "ahmed@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
+    ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Mohammed","Email"=> "mohammed@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
+    ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Sara","Email"=> "sara@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
+    ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Samira","Email"=> "samira@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"]
+  );
+?>

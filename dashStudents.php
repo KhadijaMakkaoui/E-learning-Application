9365491aab1aa051c0f393e1dad19c3f44a73274
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-01-21 12:56:44 +0100

    integrer une balise php pour afficher le tableau students lists créer une variable de type array multidimensional et à l'interieur créer 7 tableaux de type associative contenant les information Nom,Email ... Afficher les balises td du table row contenat les données de chaque tableau en utilisant deux  boucles foreach la premiere est pour boucler sur les 7 tableaux et la deuxieme est pour bouclé à l'intérieur de chaque tableau associative

--- dashStudents.php
+++ dashStudents.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="bootsrap/font/bootstrap-icons.css" />
   </head>
   <body>
-    <div class="container-fluid vh-100">
+    <div class="container-fluid ">
       <div class="row flex-nowrap">
         <!--        sidebar        -->
         <?php
@@ -23,22 +23,19 @@
           include 'Component/header.php';
       ?>
           <main class="row mx-1" style="background-color: #f8f8f8">
-            <div class="border-bottom d-flex justify-content-between">
-              <h2 class=" ">Students List</h2>
+            <div class="border-bottom d-flex justify-content-between mt-2">
+              <h3 class=" ">Students List</h3>
               <div class="">
-                <a href="#" class="link-info px-2"
+                <a href="#" class="link-info "
                   ><i class="bi bi-arrow-down-up"></i
                 ></a>
-                <input
-                  type="button"
-                  class="btn text-light"
-                  value="ADD NEW STUDENT"
-                  style="background-color: #00c1fe"
-                />
+                <a href="#" class="link-light bg-info rounded-2 px-2">
+                <i class="bi bi-person-plus-fill"></i>
+                </a>
               </div>
             </div>
-            <div class="table-responsive">
-              <table class="table">
+            <div class="table-responsive" style="max-height: 400px;">
+              <table class="table ">
                 <thead class="text-secondary">
                   <tr>
                     <th scope="col"></th>
@@ -50,8 +47,47 @@
                     <th scope="col"></th>
                   </tr>
                 </thead>
-                <tbody>
-                  <tr style="background-color: white">
+                <tbody class="bg-white">
+                  <?php 
+                    // include './Component/ListStudents.php'
+                    $studentsList = array (
+                      ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Khadija","Email"=> "makkaoui@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
+                      ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Noura","Email"=> "Noura@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
+                      ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Lamya","Email"=> "lamya@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
+                      ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Ahmed","Email"=> "ahmed@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
+                      ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Mohammed","Email"=> "mohammed@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
+                      ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Sara","Email"=> "sara@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
+                      ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Samira","Email"=> "samira@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"]
+                    );
+                    foreach ($studentsList as $std) {
+                      echo '<tr>';
+                      foreach ($std as $key => $value) {
+                          echo '<td>' . $value . '</td>'; 
+                         
+                      }
+                      echo '
+                      <td style="color: #00c1fe">
+                      <i class="bi bi-pen"></i>
+                      <i class="bi bi-trash"></i>
+                    </td>';
+                      echo '</tr>';
+                     } 
+                  ?>
+                   <!-- <tr style="background-color: white">
+                    <th scope="row">
+                      <img src="images/table-pic.svg" alt="user-pic" />
+                    </th>
+                    <td>username</td>
+                    <td>user@email.com</td>
+                    <td>0612346789</td>
+                    <td>1234567898765</td>
+                    <td>08-Dec,2021</td>
+                    <td style="color: #00c1fe">
+                      <i class="bi bi-pen"></i>
+                      <i class="bi bi-trash"></i>
+                    </td>
+                  </tr> -->
+                  <!--<tr style="background-color: white">
                     <th scope="row">
                       <img src="images/table-pic.svg" alt="user-pic" />
                     </th>
@@ -78,21 +114,7 @@
                       <i class="bi bi-pen"></i>
                       <i class="bi bi-trash"></i>
                     </td>
-                  </tr>
-                  <tr style="background-color: white">
-                    <th scope="row">
-                      <img src="images/table-pic.svg" alt="user-pic" />
-                    </th>
-                    <td>username</td>
-                    <td>user@email.com</td>
-                    <td>0612346789</td>
-                    <td>1234567898765</td>
-                    <td>08-Dec,2021</td>
-                    <td style="color: #00c1fe">
-                      <i class="bi bi-pen"></i>
-                      <i class="bi bi-trash"></i>
-                    </td>
-                  </tr>
+                  </tr> -->
                 </tbody>
               </table>
             </div>

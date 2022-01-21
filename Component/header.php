6fc39da9031afc84e9e495f348e758af888adfaf
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-01-21 21:06:08 +0100

    Accessibility Ajouter un href pour le lien de l'icone back

--- Component/header.php
+++ Component/header.php
@@ -3,13 +3,13 @@ $header=<<<navbar
 <nav class="navbar row">
                     <div class="col-2 d-flex flex-row w-100  justify-content-between ">
                         <div>
-                            <a class="link-secondary">
+                            <a href="#" class="link-secondary">
                                 <i class="fs-5 bi-caret-left-square"></i>
                             </a>
                         </div>
                         <div class="input-group" style="width:170px;">
                             <input type="text" class="form-control" placeholder="Search ..">
-                            <button class="btn btn-outline-secondary rounded-end me-2" type="button" id="button-addon2">
+                            <button class="btn btn-outline-secondary rounded-end me-2" type="button" id="button-addon2" aria-label="search button">
                                 <i class="fs-6 bi-search"></i>
                             </button>
                             <i class="fs-5 bi-bell text-secondary mt-1"></i>

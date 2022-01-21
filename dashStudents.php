ce9dc6426e3423476611055a2c8a33cad8ec25b2
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-01-21 13:10:04 +0100

    inclure le fichier ListStudents.php déplacer le tableau des données dans ListStudents

--- dashStudents.php
+++ dashStudents.php
@@ -49,21 +49,11 @@
                 </thead>
                 <tbody class="bg-white">
                   <?php 
-                    // include './Component/ListStudents.php'
-                    $studentsList = array (
-                      ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Khadija","Email"=> "makkaoui@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
-                      ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Noura","Email"=> "Noura@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
-                      ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Lamya","Email"=> "lamya@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
-                      ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Ahmed","Email"=> "ahmed@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
-                      ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Mohammed","Email"=> "mohammed@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
-                      ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Sara","Email"=> "sara@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
-                      ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Samira","Email"=> "samira@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"]
-                    );
+                     include './Component/ListStudents.php';
                     foreach ($studentsList as $std) {
                       echo '<tr>';
                       foreach ($std as $key => $value) {
                           echo '<td>' . $value . '</td>'; 
-                         
                       }
                       echo '
                       <td style="color: #00c1fe">

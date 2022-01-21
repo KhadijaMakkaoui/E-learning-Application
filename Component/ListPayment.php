aca77a45f0f6fdfc9cc4b62f0a5547adeda7b976
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-01-21 13:31:02 +0100

    Créer un fichier ListPayment.php Contient la liste des details de payement

---
+++ Component/ListPayment.php
@@ -0,0 +1,11 @@
+<?php
+$payementList = array (
+    ["Name" => "Khadija","schedual"=> "First","Bill"=> "2939393","Amount"=> "20000 DHS","Balance"=>"500,000DHS","Date_addmission"=> "20-Janvier,2022"],
+    ["Name" => "Noura","schedual"=> "First","Bill"=> "433555666","Amount"=> "20000 DHS","Balance"=>"500,000DHS","Date_addmission"=> "20-Janvier,2022"],
+    ["Name" => "Lamya","schedual"=> "First","Bill"=> "96009393","Amount"=> "20000 DHS","Balance"=>"500,000DHS","Date_addmission"=> "20-Janvier,2022"],
+    ["Name" => "Ahmed","schedual"=> "First","Bill"=> "505954","Amount"=> "20000 DHS","Balance"=>"500,000DHS","Date_addmission"=> "20-Janvier,2022"],
+    ["Name" => "Mohammed","schedual"=> "First","Bill"=> "3222225555","Amount"=> "20000 DHS","Balance"=>"500,000DHS","Date_addmission"=> "20-Janvier,2022"],
+    ["Name" => "Sara","schedual"=> "First","Bill"=> "76464","Amount"=> "20000 DHS","Balance"=>"500,000DHS","Date_addmission"=> "20-Janvier,2022"],
+    ["Name" => "Samira","schedual"=> "First","Bill"=> "4767655","Amount"=> "20000 DHS","Balance"=>"500,000DHS","Date_addmission"=> "20-Janvier,2022"],
+  );
+?>

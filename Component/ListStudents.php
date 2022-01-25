ed4032853ec13cfdea69e70d7f72ed1a84c79b54
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-01-25 10:24:11 +0100

    encode le tableau associative afficher sous forme de json

--- Component/ListStudents.php
+++ Component/ListStudents.php
@@ -8,4 +8,6 @@ $studentsList = array (
     ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Sara","Email"=> "sara@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"],
     ['img' =>'<img src="images/table-pic.svg" alt="user-pic" />',"Name" => "Samira","Email"=> "samira@gmail.com","Phone"=> "062939393","EnrollNumber"=> "3892392302019","Date_addmission"=> "20-Janvier,2022"]
   );
+
+  echo json_encode($studentsList[0]);
 ?>

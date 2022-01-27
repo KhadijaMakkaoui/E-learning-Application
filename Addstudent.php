be61a8e2302251edc2fb0ca554556a86130afddc
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-01-27 15:33:32 +0100

    Ajouter input pour importer l'image du profile input type=file

--- Addstudent.php
+++ Addstudent.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,35 +11,44 @@
     <link rel="stylesheet" href="bootsrap/font/bootstrap-icons.css">
 
 </head>
+
 <body>
-    <div class="container">
+    <div class="container my-5" style="max-width: 500px;">
         <h1 class="text-center">ADD NEW STUDENT</h1>
         <div class="text-center">
-  <img src="images/img-youcode.png" class="rounded-circle" alt="...">
-</div>
-        <form>
+            <img src="images/img-youcode.png" class="rounded-circle" alt="image apprenant">
+        </div>
+        <form action="dashStudents.php" method="GET">
+            <div class="mb-3">
+                <label for="img" class="form-label">Profil picture</label>
+                <input class="form-control" type="file" name="img">
+            </div>
             <div class="mb-3">
                 <label for="name" class="form-label">Name</label>
-                <input type="text" class="form-control" id="name" aria-describedby="Name" placeholder="Student">
+                <input type="text" class="form-control" name="name" aria-describedby="Name" placeholder="Student">
             </div>
-           <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="user@email.com">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" class="form-control" name="email" aria-describedby="emailHelp" placeholder="user@email.com">
             </div>
             <div class="mb-3">
                 <label for="phone" class="form-label">Phone</label>
-                <input type="tel" class="form-control" id="phone" aria-describedby="phone number" placeholder="XXXXXXX">
+                <input type="tel" class="form-control" name="phone" aria-describedby="phone number" placeholder="XXXXXXX">
             </div>
             <div class="mb-3">
                 <label for="EnrollNum" class="form-label">Enroll number</label>
-                <input type="number" class="form-control" id="EnrollNum" aria-describedby="Name" min="0" placeholder="12345">
+                <input type="number" class="form-control" name="EnrollNum" aria-describedby="Name" min="0" placeholder="12345">
             </div>
             <div class="mb-3">
                 <label for="dateAdd" class="form-label">Date of admission</label>
-                <input type="date" class="form-control" id="dateAdd" aria-describedby="Name" min="0" placeholder="">
+                <input type="date" class="form-control" name="dateAdd" aria-describedby="date" min="0" placeholder="">
+            </div>
+            <div class="d-grid gap-2">
+                <button type="submit" class="btn btn-info" aria-describedby="button">Info</button>
             </div>
         </form>
     </div>
 
 </body>
+
 </html>

f5540effbcc12cd536eba1435dacb83535d36fd8
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-02-04 16:48:23 +0100

    set up pour travailler avec Mysql

---
+++ dashPayment.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dashboard- Payment Details</title>
+  <meta name="description" content="Dashboard of Payment Details">
+
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="/bootsrap/bootstrap.min.css">
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+  <link rel="stylesheet" href="bootsrap/font/bootstrap-icons.css" />
+
+</head>
+
+<body>
+  <div class="container-fluid">
+    <div class="row flex-nowrap">
+      <!--      sidebar      -->
+      <?php
+      $activeHome = NULL;
+      $activeStudent = NULL;
+      $activePay = "active";
+      include 'Component/sidebar.php';
+      ?>
+      <!--          content area         -->
+      <div class="col-9 col-md-9 col-lg-9 col-xl-10">
+        <?php
+        include 'Component/header.php';
+        ?>
+        <main class="row" style="background-color: #f8f8f8">
+          <div class="border-bottom d-flex justify-content-between">
+            <h2>Payment Details</h2>
+            <div>
+              <a href="#" class="link-info px-2"><i class="bi bi-arrow-down-up"></i></a>
+            </div>
+
+          </div>
+          <div class="table-responsive " style="max-height: 400px;">
+
+            <table class="table table-striped">
+              <thead class="text-secondary">
+                <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">Payement Schedule</th>
+                  <th scope="col">Bill Number</th>
+                  <th scope="col">Amount Paid</th>
+                  <th scope="col">Balance amount</th>
+                  <th scope="col">Date of admission</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                //  include './Component/ListPayment.php';
+                $json = file_get_contents('./json/payments.json');
+                $payementList = json_decode($json);
+                ?>
+                <?php foreach ($payementList as $detail) : ?>
+                  <tr>
+
+                    <?php foreach ($detail as $key => $value) : ?>
+                      <td> <?php echo $value ?></td>
+                    <?php endforeach; ?>
+
+                    <td style="color: #00c1fe"> <i class="bi bi-eye"></i></td>
+                  </tr>
+                   
+                <?php endforeach; ?>
+              </tbody>
+            </table>
+          </div>
+        </main>
+      </div>
+    </div>
+  </div>
+  <script src="/bootsrap/bootstrap.min.js"></script>
+  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
+
+</body>
+
+</html>
---
+++ dashStudents.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dashboard- Students List</title>
+    <meta name="description" content="Dashboard of Students List">
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="/bootsrap/bootstrap.min.css" />
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="bootsrap/font/bootstrap-icons.css" />
+  </head>
+  <body>
+    <div class="container-fluid ">
+      <div class="row flex-nowrap">
+        <!--        sidebar        -->
+        <?php
+          $activeHome=NULL;
+          $activeStudent="active";
+          $activePay=NULL;
+          include 'Component/sidebar.php';
+      ?>
+        <!--        content area         -->
+        <div class="col-9 col-md-9 col-lg-9 col-xl-10">
+        <?php
+          include 'Component/header.php';
+      ?>
+          <main class="row mx-1" style="background-color: #f8f8f8">
+            <div class="border-bottom d-flex justify-content-between mt-2">
+              <h3 class=" ">Students List</h3>
+              <div class="">
+                <a href="#" class="link-info "
+                  ><i class="bi bi-arrow-down-up"></i
+                ></a>
+                <a href="#" class="link-light bg-info rounded-2 px-2">
+                <i class="bi bi-person-plus-fill"></i>
+                </a>
+              </div>
+            </div>
+            <div class="table-responsive" style="max-height: 400px;">
+              <table class="table ">
+                <thead class="text-secondary">
+                  <tr>
+                    <th scope="col"></th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Enroll Number</th>
+                    <th scope="col">Date of admission</th>
+                    <th scope="col"></th>
+                  </tr>
+                </thead>
+                <tbody class="bg-white">
+                  <!-- Afficher la list des apprenants -->
+                  <?php
+                    $json = file_get_contents('./json/students.json');
+
+                    $studentsList=json_decode($json,true);                   
+                ?>
+                <?php foreach ($studentsList as $std) : ?>
+                  <tr>
+                    <td><img src="<?php echo $std['img'] ?>" alt="image student"></td>
+                    <td> <?php echo $std['Name'] ?></td>
+                    <td> <?php echo $std['Email'] ?></td>
+                    <td> <?php echo $std['Phone'] ?></td>
+                    <td> <?php echo $std['EnrollNumber'] ?></td>
+                    <td> <?php echo $std['Date_addmission'] ?></td>
+                    <td style="color: #00c1fe">
+                      <a href="#" style="color: unset;"><i class="bi bi-pen"></i></a>
+                      <a href="#" style="color: unset;"><i class="bi bi-trash"></i></a>
+                    </td>
+                  </tr>
+                  
+                <?php endforeach; ?>
+                  
+              <!-- Add new student -->
+
+                </tbody>
+              </table>
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
+
+    <script src="/bootsrap/bootstrap.min.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
+  </body>
+</html>

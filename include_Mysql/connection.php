92236a0bd689a9ec333a2f88bbc4c32f71139aec
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-02-05 15:54:45 +0100

    modifier le nom de la base de données

--- include_Mysql/connection.php
+++ include_Mysql/connection.php
@@ -2,9 +2,10 @@
 $servername="localhost";
 $username="root";
 $password="";
+$dbname="e_classe_db";
 
 try {
-    $conn=mysqli_connect($servername,$username,$password);
+    $conn=mysqli_connect($servername,$username,$passwordCr,$dbname);
 } catch (Exception $e) {
     echo "Connection failed: " . $e->getMessage();
 }

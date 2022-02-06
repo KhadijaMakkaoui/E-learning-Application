64d5d6e4ffac995df82a55d2e166fd9f76b17aa5
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-02-06 21:07:07 +0100

    integrer un formulaire pour l'ajout créer un formulaire dans un modal bootsrap sous form de popup contenat les champs pour saisir les données de l'apprenant

---
+++ add.php
@@ -0,0 +1,45 @@
+
+    <div class="container" >
+        <!-- Button trigger modal -->
+        <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Launch demo modal
+        </button> -->
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" aria-labelledby="addForm" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addForm">Addition Form</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                     <div class="modal-body">
+                        <form method="POST" action="" >
+                       
+                            <div class="form-group mb-2">
+                                <input type="text" name="Name" class="form-control" placeholder="Enter Name..." required="">
+                            </div><!-- form-group -->
+                            <div class="form-group mb-2">
+                                <input type="text" name="Email" class="form-control" placeholder="Enter Email..." required="">
+                            </div><!-- form-group -->
+                            <div class="form-group mb-2">
+                                <input type="text" name="Phone" class="form-control" placeholder="Enter Phone..." required="">
+                            </div><!-- form-group -->
+                            <div class="form-group mb-2">
+                                <input type="text" name="Enroll_Number" class="form-control" placeholder="Enter Enroll Number..." required="">
+                            </div><!-- form-group -->
+                            <div class="form-group mb-2">
+                                <input type="text" name="Date_addmission" class="form-control" placeholder="Enter Date addmission..." required="">
+                            </div>
+
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary btn-info">Add</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>

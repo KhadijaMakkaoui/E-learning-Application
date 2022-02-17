71f3f318d1c4eb904436cb50e4a4df734b629390
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-02-17 14:50:27 +0100

    Tester si le temps d'expiration egale au temps actuel si c'est le cas le logout sera appliqué

--- checkLogin.php
+++ checkLogin.php
@@ -2,4 +2,8 @@
 if(!(isset($_SESSION['isLogin']) && $_SESSION['isLogin']==true))
 header("location: index.php");
 
+if($_SESSION['expireTime']==time()){
+  header("location: logout.php");
+    
+}
 ?>
